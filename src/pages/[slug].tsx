71d2dab79b79a8c697414e89a251133a0e258881
James Milton <james.milton@me.com>
2023-04-21 20:30:00 +0200

    Profile page with ssg helper

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,23 +1,70 @@
-import { type NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import Image from "next/image";
 import { api } from "~/utils/api";
+import { createServerSideHelpers } from "@trpc/react-query/server";
+import { appRouter } from "~/server/api/root";
+import { prisma } from "~/server/db";
+import superjson from "superjson";
+import { PageLayout } from "~/components/layout";
 
-const ProfilePage: NextPage = () => {
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "jamesxdigital",
+    username,
   });
-  if (isLoading) return <div>Loading...</div>;
   if (!data) return <div>Not found</div>;
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data.username}</title>
       </Head>
-      <main className="flex justify-center">
-        <div>{data.username}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 border-b border-slate-400 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile image}`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-4 -mb-[64px] rounded-full border-2 border-slate-400 bg-black"
+          />
+        </div>
+        <div className="h-[64px]"></div>
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const helpers = createServerSideHelpers({
+    router: appRouter,
+    ctx: { prisma, userId: null },
+    transformer: superjson, // optional - adds superjson serialization
+  });
+
+  const slug = context.params?.slug;
+
+  if (typeof slug !== "string") {
+    throw new Error("no slug");
+  }
+
+  const username = slug.replace("@", "");
+
+  await helpers.profile.getUserByUsername.prefetch({ username });
+
+  return {
+    props: {
+      trpcState: helpers.dehydrate(),
+      username,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default ProfilePage;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import Link from "next/link";
+import { PageLayout } from "~/components/layout";
 
 dayjs.extend(relativeTime);
 const CreatePostWizard = () => {
@@ -134,19 +135,17 @@ const Home: NextPage = () => {
   if (!userLoaded) return <div />;
 
   return (
-    <main className="flex justify-center">
-      <div className="border-slate-4 00 h-screen w-full border-x md:max-w-2xl">
-        <div className="flex border-b border-slate-400 p-4">
-          {!isSignedIn && (
-            <div className="flex justify-center">
-              <SignInButton />
-            </div>
-          )}
-          {!!isSignedIn && <CreatePostWizard />}
-        </div>
-        <Feed />
+    <PageLayout>
+      <div className="flex border-b border-slate-400 p-4">
+        {!isSignedIn && (
+          <div className="flex justify-center">
+            <SignInButton />
+          </div>
+        )}
+        {!!isSignedIn && <CreatePostWizard />}
       </div>
-    </main>
+      <Feed />
+    </PageLayout>
   );
 };
 
